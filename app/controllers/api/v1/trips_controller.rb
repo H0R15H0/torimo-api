18b729624e3d1ee665ea935f98d8ddf504cca8c5
H0R15H0 <shohori13@gmail.com>
2021-03-13 23:39:47 +0900

    tripの取得APIを実装。

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::TripsController < Api::V1::BaseController
 
   def index
-    trip = Trip.includes(trip_courses: [:transportation_to_next]).all.limit(5)
+    trips = Trip.includes(trip_courses: [:transportation_to_next]).all.limit(5)
+    render json: {trips: trips}, include: [trip_courses: {include: [:transportation_to_next]}]
+  end
+
+  def show
+    trip = Trip.includes(trip_courses: [:transportation_to_next]).find(params[:id])
     render json: {trip: trip}, include: [trip_courses: {include: [:transportation_to_next]}]
   end
 
