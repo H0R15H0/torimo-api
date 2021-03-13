66d8c5303d22cef036a9ed9db8c90f3b5a980d4c
H0R15H0 <shohori13@gmail.com>
2021-03-13 22:15:57 +0900

    trip周りのrouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      # define api endpoints
+      get 'trips' => 'trips#index'
+      get 'trips/:id' => 'trips#show'
+    end
+  end
 end
