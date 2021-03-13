10a1a7b88d293eceb3112d0295ba8aea83477619
H0R15H0 <shohori13@gmail.com>
2021-03-13 23:48:15 +0900

    seedを追加。

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,12 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(token: "token")
+trip = Trip.create(user_id: u.id, main_place: "都道府県", budget: 50000, title: "sample title")
+TripCourse.create(trip_id: trip.id, place: "都道府県", arrival_time: Time.now, day_of_trip: 1, index_in_course: 1)
+TripCourse.create(trip_id: trip.id, place: "都道府県", arrival_time: Time.now, day_of_trip: 1, index_in_course: 2)
+TripCourse.create(trip_id: trip.id, place: "都道府県", arrival_time: Time.now, day_of_trip: 2, index_in_course: 3)
+Transportation.create(name: "車")
+Transportation.create(name: "徒歩")
+TripCourseTransportation.create(from_id: 1, to_id: 2, transportation_id: 1)
+TripCourseTransportation.create(from_id: 2, to_id: 3, transportation_id: 2)
