32b437e1f1c621c08e87309b2b709f8caa20004c
H0R15H0 <shohori13@gmail.com>
2021-03-14 15:13:07 +0900

    tripから交通手段を参照できるようにした。trip.transportations

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::TripsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
   def index
-    trips = Trip.includes(trip_courses: [:transportation_to_next]).all.limit(10)
-    render json: {trips: trips}, include: [trip_courses: {include: [:transportation_to_next]}]
+    trips = Trip.includes(:transportations, trip_courses: [:transportation_to_next]).all.limit(10)
+    render json: {trips: trips}, include: [:transportations, trip_courses: {include: [:transportation_to_next]}]
   end
 
   def show
-    trip = Trip.includes(trip_courses: [:transportation_to_next]).find(params[:id])
-    render json: {trip: trip}, include: [trip_courses: {include: [:transportation_to_next]}]
+    trip = Trip.includes(:transportations, trip_courses: [:transportation_to_next]).find(params[:id])
+    render json: {trip: trip}, include: [:transportations, trip_courses: {include: [:transportation_to_next]}]
   end
 
   def search
     p params
-    trips = Trip.includes(trip_courses: [:transportation_to_next]).where("title LIKE ?", "%#{params[:q]}%") 
-    render json: {trips: trips}, include: [trip_courses: {include: [:transportation_to_next]}]
+    trips = Trip.includes(:transportations, trip_courses: [:transportation_to_next]).where("title LIKE ?", "%#{params[:q]}%") 
+    render json: {trips: trips}, include: [:transportations, trip_courses: {include: [:transportation_to_next]}]
   end
 
   def create
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ApplicationRecord
   has_many :trip_courses
   belongs_to :user
+  has_many :transportations, through: :trip_courses, source: :transportation_to_next
 
   accepts_nested_attributes_for :trip_courses, allow_destroy: true
 end
